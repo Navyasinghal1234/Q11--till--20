0f89a90a0c392b8b124823ed7a4ea197bbc29f1d
Navyasinghal1234 <write24navya@gmail.com>
2025-11-12 15:29:49 +0530

    q17.c

---
+++ q17.c
@@ -0,0 +1,27 @@
+//WAP to find the roots of quadratic equation and categorize them.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c, d, r1, r2;
+
+    printf("Enter a, b, c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    d = b*b - 4*a*c;  // discriminant
+
+    if (d > 0) {
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
+        printf("Roots are real and different.\n");
+    }
+    else if (d == 0) {
+        r1 = r2 = -b / (2*a);
+        printf("Roots are real and equal.\n");
+    }
+    else {
+        printf("Roots are imaginary.\n");
+    }
+
+    return 0;
+}
